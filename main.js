f145a7d82770751ff04326d50a2ec70caf5d4670
Janis Karasa <jkarasa@gmail.com>
2023-07-21 15:06:39 +0200

    Initializing AOS - Animation On Scroll Library in main.js file and adding AOS Attributes to respective elements - hero__right and hero__left in index.html file's Hero section

--- main.js
+++ main.js
@@ -44,3 +44,23 @@ navCloseElement.addEventListener("click", navClose);
 navBgOverlay.addEventListener("click", navClose);
 
 // !!! LINK THIS JS with HTML index.html file - on the bottom of body element
+
+// AOS
+// Initializing AOS - Animation On Scroll library. More info on their website - documentation
+AOS.init({
+  // offset - 200 for the scroll - 200px down from the top then animation will start (offset for anchorPlacement: 'top-bottom')
+  offset: 200,
+  // delay - 100 for the animation
+  delay: 100,
+  // duration - for animation - 400
+  duration: 400,
+  easing: "ease",
+  // once - false means that animation will not happen once but as many times as we scroll back and forth
+  once: "false",
+  mirror: false,
+  // anchorPlacement means when/where the animation starts - whenever elements 'top' and the viewpoint 'bottom' (screen bottom) will meet then the animation will start.
+  anchorPlacement: "top-bottom",
+});
+// !!! NOW ADD SOME ATTRIBUTES in HERO SECTION:
+// next to 'hero__left' class we add 'data-aos="fade-left"'
+// next to 'hero__right' class we add 'data-aos="fade-right"'
